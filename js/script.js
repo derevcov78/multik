beb5e5c33b094ede0288f4ee2a7c8350a3f1fba5
derevcov78 <79917945+derevcov78@users.noreply.github.com>
2021-04-07 19:28:41 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,88 @@
+$(document).ready(function () {
+    $("form").submit(function () {
+        // Получение ID формы
+        var formID = $(this).attr('id');
+        // Добавление решётки к имени ID
+        var formNm = $('#' + formID);
+        $.ajax({
+            type: "POST",
+            url: 'mail.php',
+            data: formNm.serialize(),
+            success: function (data) {
+                // Вывод текста результата отправки
+                $(formNm).html(data);
+            },
+            error: function (jqXHR, text, error) {
+                // Вывод текста ошибки отправки
+                $(formNm).html(error);
+            }
+        });
+        return false;
+    });
+});
+
+
+$(document).ready(function () {
+    $('.block__title').click(function (event) {
+        if ($('.block').hasClass('one')) {
+            $('.block__title').not($(this)).removeClass('active');
+            $('.block__text').not($(this).next()).slideUp(300);
+        }
+        $(this).toggleClass('active').next().slideToggle(300);
+    });
+});
+
+/*плавный переход к нужным блокам на странице*/
+$(function () {
+
+
+    /* Fixed Header */
+    let header = $("#header");
+    let intro = $("#intro");
+    let introH = intro.innerHeight();
+    let scrollPos = $(window).scrollTop();
+    let nav = $("#nav");
+    let navToggle = $("#navToggle");
+
+    checkScroll(scrollPos, introH);
+
+    $(window).on("scroll resize", function () {
+        introH = intro.innerHeight();
+        scrollPos = $(this).scrollTop();
+
+        checkScroll(scrollPos, introH);
+    });
+
+    function checkScroll(scrollPos, introH) {
+        if (scrollPos > introH) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+    }
+
+
+
+    /* Smooth scroll */
+    $("[data-scroll]").on("click", function (event) {
+        event.preventDefault();
+
+        let elementId = $(this).data('scroll');
+        let elementOffset = $(elementId).offset().top;
+
+        nav.removeClass("show");
+
+        $("html, body").animate({
+            scrollTop: elementOffset - 50
+        }, 700);
+    });
+
+
+
+    /* Nav Toggle */
+    navToggle.on("click", function (event) {
+        event.preventDefault();
+
+        nav.toggleClass("show");
+    });
+});
